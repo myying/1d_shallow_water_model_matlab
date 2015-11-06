27eb028cc6f0b19b58bc1dcf95fd85ba5ae1bcca
Yue (Michael) Ying <hardyying@gmail.com>
2015-11-06 16:32:22 -0500

    added numerical schemes FTCS, FTBS; added profiling

--- shallow_water_model.m
+++ shallow_water_model.m
@@ -17,62 +17,98 @@ g=9.8;      %gravity
 f=0.0001;   %Coriolis
 kdif=10;    %diffuction coef
 
+numerics=3; %1:CTCS, 2:FTCS, 3: FTBS numerical schemes
+
 nifcor=0;   %if 1, include Coriolis
 nifwind=0;  %if 1, include other terms besides Coriolis in u,v eqns.
 nifdif=0;   %if 1, include diffusion
 nifad=1;    %if 1, include advection
 
 scale_factor=nx;       %scaling factor for plotting wind vectors
-show_animation=1;      %if 1, show animation
+show_animation=1;      %if 1, show animation; turn this off when profiling.
 animation_stride=1;    %if =n, skip every n frames
 animation_delay=0.001; %deley in between frame in seconds
 
-%initialize
-dt2=dt*2; dx2=dx*2; diffac=kdif*dt/dx/dx;
-time=0;  %time in seconds
+%initial condition
 u(1:nx)=vel;
 v(1:nx)=0.0; 
 h(1:nx)=hbar;
 ind=ceil(hc-hw/2):floor(hc+hw/2);
 h(ind)=hbar+amp*(1+cos(2*pi*(ind-hc)/hw));
-ub=u; vb=v; hb=h;
-ua=u; va=v; ha=h;
 h0=h;
-loc1=find(h==max(h),1); maxh(1)=max(h);
+loc1=find(h==max(h),1); 
+maxh(1:nt+1)=NaN; maxh(1)=max(h);
 x(1:nx)=(1:nx)*dx/1000;
-dtcalc=dt;
+
+figure('Position',[100 100 560 560]);
 
 %run model
-figure('Position',[100 100 560 560]);
+time=0;  %time in seconds
+t_start=cputime;
+
 for n=1:nt
-  %CTCS scheme (leapfrog)
   j=2:nx-1;
-  ha(j)=hb(j)-nifad*u(j).*(dtcalc/dx2).*(h(j+1)-h(j-1)) ...
-             -h(j).*(dtcalc/dx2).*(u(j+1)-u(j-1)) ...
-             +nifdif*diffac*(hb(j+1)-2*hb(j)+hb(j-1));
-  ua(j)=ub(j)-nifwind*nifad*u(j).*(dtcalc/dx2).*(u(j+1)-u(j-1)) ...
-             -nifwind*(g*dtcalc/dx2)*(h(j+1)-h(j-1)) ...
-             +nifcor*f*v(j)*dtcalc ...
-             +nifwind*nifdif*diffac*(ub(j+1)-2*ub(j)+ub(j-1));
-  va(j)=vb(j)-nifwind*nifad*u(j).*(dtcalc/dx2).*(v(j+1)-v(j-1)) ...
-             -nifcor*f*u(j)*dtcalc ...
-             +nifwind*nifdif*diffac*(vb(j+1)-2*vb(j)+vb(j-1));
+  if(numerics==1) %CTCS scheme (leapfrog)
+    if(n==1)
+      ub=u; vb=v; hb=h;
+      ua=u; va=v; ha=h;
+      dtcalc=dt;
+    else
+      dtcalc=dt*2;
+    end
+    diffac=kdif*dtcalc/dx/dx;
+    ha(j)=hb(j)-nifad*u(j).*(dtcalc/(dx*2)).*(h(j+1)-h(j-1)) ...
+               -h(j).*(dtcalc/(dx*2)).*(u(j+1)-u(j-1)) ...
+               +nifdif*diffac*(h(j+1)-2*h(j)+h(j-1));
+    ua(j)=ub(j)-nifwind*nifad*u(j).*(dtcalc/(dx*2)).*(u(j+1)-u(j-1)) ...
+               -nifwind*(g*dtcalc/(dx*2))*(h(j+1)-h(j-1)) ...
+               +nifcor*f*v(j)*dtcalc ...
+               +nifwind*nifdif*diffac*(u(j+1)-2*u(j)+u(j-1));
+    va(j)=vb(j)-nifwind*nifad*u(j).*(dtcalc/(dx*2)).*(v(j+1)-v(j-1)) ...
+               -nifcor*f*u(j)*dtcalc ...
+               +nifwind*nifdif*diffac*(v(j+1)-2*v(j)+v(j-1));
+    ub=u; u=ua;
+    vb=v; v=va;
+    hb=h; h=ha;
 
-  %boundary condition (cyclic)
-  ua(1)=ua(nx-1); ua(nx)=ua(2);
-  va(1)=va(nx-1); va(nx)=va(2);
-  ha(1)=ha(nx-1); ha(nx)=ha(2);
-
-  %update
-  ub=u; u=ua;
-  vb=v; v=va;
-  hb=h; h=ha;
+  elseif(numerics==2) %FTCS scheme
+    diffac=kdif*dt/dx/dx;
+    ha(j)=h(j)-nifad*u(j).*(dt/(dx*2)).*(h(j+1)-h(j-1)) ...
+              -h(j).*(dt/(dx*2)).*(u(j+1)-u(j-1)) ...
+              +nifdif*diffac*(h(j+1)-2*h(j)+h(j-1));
+    ua(j)=u(j)-nifwind*nifad*u(j).*(dt/(dx*2)).*(u(j+1)-u(j-1)) ...
+              -nifwind*(g*dt/(dx*2))*(h(j+1)-h(j-1)) ...
+              +nifcor*f*v(j)*dt ...
+              +nifwind*nifdif*diffac*(u(j+1)-2*u(j)+u(j-1));
+    va(j)=v(j)-nifwind*nifad*u(j).*(dt/(dx*2)).*(v(j+1)-v(j-1)) ...
+              -nifcor*f*u(j)*dt ...
+              +nifwind*nifdif*diffac*(v(j+1)-2*v(j)+v(j-1));
+    h=ha; u=ua; v=va;
+  
+  elseif(numerics==3) %FTBS scheme
+    diffac=kdif*dt/dx/dx;
+    ha(j)=h(j)-nifad*u(j).*(dt/dx).*(h(j)-h(j-1)) ...
+              -h(j).*(dt/dx).*(u(j)-u(j-1)) ...
+              +nifdif*diffac*(h(j+1)-2*h(j)+h(j-1));
+    ua(j)=u(j)-nifwind*nifad*u(j).*(dt/dx).*(u(j)-u(j-1)) ...
+              -nifwind*(g*dt/dx)*(h(j)-h(j-1)) ...
+              +nifcor*f*v(j)*dt ...
+              +nifwind*nifdif*diffac*(u(j+1)-2*u(j)+u(j-1));
+    va(j)=v(j)-nifwind*nifad*u(j).*(dt/dx).*(v(j)-v(j-1)) ...
+              -nifcor*f*u(j)*dt ...
+              +nifwind*nifdif*diffac*(v(j+1)-2*v(j)+v(j-1));
+    h=ha; u=ua; v=va;
+  end
+  
   time=time+dt;
-  dtcalc=dt2;
-  diffac=kdif*dtcalc/dx/dx;
+  
+  %boundary condition (cyclic)
+  u(1)=u(nx-1); u(nx)=u(2);
+  v(1)=v(nx-1); v(nx)=v(2);
+  h(1)=h(nx-1); h(nx)=h(2);
 
   %plot results
-  if(show_animation==1 && mod(time,animation_stride*dt)==0)
+  if(show_animation==1 && mod(n,animation_stride)==0)
     subplot(2,1,1)
     plot(x,h);
     %analytic solution for advection equation
@@ -103,3 +139,5 @@ for n=1:nt
     disp(['model exploded at t=' num2str(time) 's (' num2str(n) ' steps)']); break; 
   end
 end
+t_end=cputime;
+disp(['It takes ' num2str(t_end-t_start) ' s']);
